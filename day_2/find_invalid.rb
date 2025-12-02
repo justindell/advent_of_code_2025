aef7c3a5cde945e44c2e1039ec7d4b2996da1a6f
Justin Dell <justin.dell@healthstrategyllc.com>
2025-12-02 08:34:06 -0600

    day 2 part 1

---
+++ day_2/find_invalid.rb
@@ -0,0 +1,33 @@
+class ProductId
+  def self.from_range(string)
+    Range.new(*string.split("-").map(&:to_i)).to_a.map { |product_id| new(product_id) }
+  end
+
+  def initialize(id)
+    @id = id.to_s
+  end
+
+  def id = @id.to_i
+
+  def invalid?
+    return false unless size.even?
+
+    left_side == right_side
+  end
+
+  private
+
+  def left_side = @id[0..(half_size - 1)]
+  def right_side = @id[half_size..-1]
+  def size = @id.size
+  def half_size = size / 2
+end
+
+puts File.read("input.txt")
+  .chomp
+  .split(",")
+  .map { |range| ProductId.from_range(range) }
+  .flatten
+  .select(&:invalid?)
+  .map(&:id)
+  .sum
